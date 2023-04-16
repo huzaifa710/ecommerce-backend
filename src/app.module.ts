4ce46d763e2cb847dfaa252a70bf67fb1c921050
huzaifa710 <58044615+huzaifa710@users.noreply.github.com>
2023-04-16 16:45:55 +0530

    new changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,37 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Product } from './products/product.entity';
 // import { CorsMiddleware } from './middleware/cors.middleware';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      database: 'ecommerce',
-      username: 'postgres',
-      password: 'postgres',
-      entities: [Product],
-      autoLoadEntities: true,
-      synchronize: true
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('STAGE') ==='prod';        
+        return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false} : null,
+          },
+          type: 'postgres',
+          entities: [Product],
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          // url: configService.get('DB_URL')
+        };
+      },
     }),
     ProductsModule,
   ],
