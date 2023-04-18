de8ffd80db00283cc4a29b65da1bece94ef75230
huzaifa710 <58044615+huzaifa710@users.noreply.github.com>
2023-04-18 17:44:16 +0530

    url fix

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
-    res.header('Access-Control-Allow-Origin', 'https://tubular-dragon-cecba9.netlify.app');
+    res.header('Access-Control-Allow-Origin', 'https://main--tubular-dragon-cecba9.netlify.app');
     res.header('Access-Control-Allow-Methods', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
