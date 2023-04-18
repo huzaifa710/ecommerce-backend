8be65215a549dd56d991d43cf8f27a6a52088cbd
huzaifa710 <58044615+huzaifa710@users.noreply.github.com>
2023-04-18 17:52:36 +0530

    preflight changes

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -4,9 +4,17 @@ import { Request, Response } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
-    res.header('Access-Control-Allow-Origin', 'https://main--tubular-dragon-cecba9.netlify.app');
+    res.header('Access-Control-Allow-Origin', 'https://main--tubular-dragon-cecba9.netlify.app'); // Update with your desired origin URL
     res.header('Access-Control-Allow-Methods', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // Handle CORS preflight requests
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Max-Age', '86400');
+      res.status(200).send();
+      return;
+    }
+
     next();
   }
 }
